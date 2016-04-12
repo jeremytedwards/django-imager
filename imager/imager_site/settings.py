884273a16cdfed4e7438d6ee72938e24c6e15031
Jeremy Edwards <jeremytedwards@gmail.com>
2016-04-12 12:05:50 -0700

    Added /templates to the app directories

--- imager/imager_site/settings.py
+++ imager/imager_site/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'imager_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'imager_site', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
