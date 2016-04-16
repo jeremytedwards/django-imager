e20aaffc22011a3f722fcc19e2f8ace5950f03e6
Jeremy Edwards <jeremytedwards@gmail.com>
2016-04-15 19:02:11 -0700

    Adjustments to models to align better with the assignment.

--- imager/imager_images/models.py
+++ imager/imager_images/models.py
@@ -2,64 +2,60 @@
 from __future__ import unicode_literals
 from django.conf import settings
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+
+# [('value', 'visible')]
+PUBLISH_CHOICES = (
+    ('public', 'Public'),
+    ('private', 'Private'),
+    ('shared', 'Shared'),
+)
 
 
 def user_file_store_path(instance, filename):
     """File will be uploaded to MEDIA_ROOT/user_{id}/{filename}"""
     return "user_{0}/{1}".format(instance.user.id, filename)
 
-# [('value', 'visible')]
-PUB_CHOICES = [(-1, 'Private'), (0, 'Shared'), (1, 'Public')]
+@python_2_unicode_compatible
+class Photo(models.Model):
+    """Create photos for a user."""
 
-class PostedImage(models.Model):
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name='posted_images'
-    )
-    file = models.ImageField(
-        upload_to=user_file_store_path
-    )
-    # title = models.
-    # description = models.
-    # date_uploaded = models.DateField(auto_now_add=True)
-    # date_modified = models.DateField(auto_now=True)
-    # date_published = models.DateField() # set in the UI
-    published = models.CharField(
-        max_length=3,
-        choices=PUB_CHOICES,
-        default=-1
-    )
-
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
+                              on_delete=models.CASCADE, related_name="photo")
+    title = models.CharField(max_length=30, blank=True)
+    description = models.CharField(max_length=60, blank=True)
+    file = models.ImageField(upload_to=user_file_store_path)
+    date_uploaded = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    date_published = models.DateTimeField(null=True, blank=True)
+    published = models.CharField(max_length=30,
+                                 choices=PUBLISH_CHOICES,
+                                 default='private')
 
     def __str__(self):
-        # TODO: build the object here?
-        return self.title
+        """Return the ownership and name of photo."""
+        return "{}'s photo of {}".format(self.owner.username, self.title)
 
-# Users should be able to designate one contained photo as the ‘cover’ for the album.
-# The albums created by a user may contain only Photos created by that same user.
-class ImageAlbum(models.Model):
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name='image_albums'
-    )
-    images = models.ManyToManyField(
-        PostedImage,
-        related_name='albums'
-    )
-    # cover = models.ManyToManyField(
-    #     PostedImage,
-    #     related_name='cover'
-    # )
-    # title = models.
-    # description = models.
-    # date_uploaded = models.DateField(auto_now_add=True)
-    # date_modified = models.DateField(auto_now=True)
-    # date_published = models.DateField()
-    # published = (‘private’, ‘shared’, ‘public’)
+
+@python_2_unicode_compatible
+class Album(models.Model):
+    """Define album class."""
+
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
+                              on_delete=models.CASCADE,
+                              related_name="album")
+    pictures = models.ManyToManyField(Photo, related_name="pictures")
+    cover = models.ForeignKey(Photo, related_name="cover_of")
+    title = models.CharField(max_length=30, blank=True)
+    description = models.CharField(max_length=60, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    date_published = models.DateTimeField(null=True, blank=True)
+    published = models.CharField(max_length=30,
+                                 choices=PUBLISH_CHOICES,
+                                 default='private')
 
     def __str__(self):
-        return self.title
-
-
+        """Return the user's album name."""
+        return "{}'s album of {}".format(self.owner.username, self.title)
